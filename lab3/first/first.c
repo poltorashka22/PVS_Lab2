6ccf628d568d6c56d4d7994e2e6ea55ee2ba46e5
HollyGooffy <nikita.byc@mail.ru>
2025-06-15 15:09:04 +0500

    lab_3

--- lab3/first/first.c
+++ lab3/first/first.c
@@ -1,3 +1,61 @@
-//
-// Created by Holly Gooffy on 21.05.2025.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  printf("1. \n");
+    int totalElements = 200000;
+    int userOption;
+
+    while (userOption = getopt(argc, argv, "n:")) {
+        if (userOption == -1) break;
+
+        switch (userOption) {
+            case 'n':
+                totalElements = atoi(optarg);
+            if (totalElements <= 0) {
+                fprintf(stderr, "Invalid input: Size must be positive.\n");
+                exit(EXIT_FAILURE);
+            }
+            break;
+            default:
+                fprintf(stderr, "Usage: %s [-n element_count]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    double *dataArray = malloc(totalElements * sizeof(*dataArray));
+    if (!dataArray) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    double sequentialSum = 0.0, parallelSum = 0.0;
+
+    srand((unsigned int)(time(NULL) ^ getpid()));
+    for (int idx = 0; idx < totalElements; idx++) {
+        dataArray[idx] = (double)rand() / (double)RAND_MAX;
+    }
+
+    double seqStartTime = omp_get_wtime();
+    for (int idx = 0; idx < totalElements; idx++) {
+        sequentialSum += dataArray[idx];
+    }
+    double seqDuration = omp_get_wtime() - seqStartTime;
+
+    double parStartTime = omp_get_wtime();
+
+#pragma omp parallel for reduction(+:parallelSum)
+    for (int idx = 0; idx < totalElements; idx++) {
+        parallelSum += dataArray[idx];
+    }
+    double parDuration = omp_get_wtime() - parStartTime;
+
+    printf("Sequential processing time: %.5f s\n", seqDuration);
+    printf("Parallel processing time:   %.5f s\n", parDuration);
+
+    free(dataArray);
+    return EXIT_SUCCESS;
+}
--- lab3/second/second.c
+++ lab3/second/second.c
@@ -1,4 +1,100 @@
-//
-// Created by Holly Gooffy on 21.05.2025.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <sys/time.h>
 
+int is_sorted(int *arr, int n) {
+    for (int i = 1; i < n; i++) {
+        if (arr[i-1] > arr[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void bubble_sort(int *arr, int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void parallel_bubble_sort(int *arr, int n) {
+    int phase, i, temp;
+    for (phase = 0; phase < n; phase++) {
+        if (phase % 2 == 0) {
+            #pragma omp parallel for private(i, temp)
+            for (i = 0; i < n/2; i++) {
+                int left = 2*i;
+                int right = 2*i + 1;
+                if (right < n && arr[left] > arr[right]) {
+                    temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                }
+            }
+        } else {
+            #pragma omp parallel for private(i, temp)
+            for (i = 0; i < (n-1)/2; i++) {
+                int left = 2*i + 1;
+                int right = 2*i + 2;
+                if (right < n && arr[left] > arr[right]) {
+                    temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int n = 200000;
+    int *arr_seq = malloc(n * sizeof(int));
+    int *arr_parallel = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        arr_seq[i] = arr_parallel[i] = rand();
+    }
+
+    struct timeval start_seq, end_seq;
+    gettimeofday(&start_seq, NULL);
+    bubble_sort(arr_seq, n);
+    gettimeofday(&end_seq, NULL);
+    double time_seq = (end_seq.tv_sec - start_seq.tv_sec) +
+                      (end_seq.tv_usec - start_seq.tv_usec)/1e6;
+    printf("Sequential time: %.6f seconds\n", time_seq);
+
+    if (!is_sorted(arr_seq, n)) {
+        printf("Sequential sort failed!\n");
+    }
+
+    struct timeval start_par, end_par;
+    gettimeofday(&start_par, NULL);
+    parallel_bubble_sort(arr_parallel, n);
+    gettimeofday(&end_par, NULL);
+    double time_par = (end_par.tv_sec - start_par.tv_sec) +
+                      (end_par.tv_usec - start_par.tv_usec)/1e6;
+    printf("Parallel time: %.6f seconds\n", time_par);
+
+    if (!is_sorted(arr_parallel, n)) {
+        printf("Parallel sort failed!\n");
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (arr_seq[i] != arr_parallel[i]) {
+            printf("Mismatch at index %d\n", i);
+            break;
+        }
+    }
+
+    free(arr_seq);
+    free(arr_parallel);
+    return 0;
+}
